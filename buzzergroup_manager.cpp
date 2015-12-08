d16bfb0b4beb368ac6ccf1447f26b2bd4f02fbeb
Manuel Vögele <develop@manuel-voegele.de>
2015-12-08 12:40:14 +0100

    Make catch strings during buzzergroup initialisation

--- buzzergroup_manager.cpp
+++ buzzergroup_manager.cpp
@@ -11,26 +11,33 @@ void buzzergroup_manager::connect(std::string device, device_type type)
     // Check if the device exists
     if (buzzergroups.count(device) == 0)
     {
-        // If not connect it
-        buzzergroup *group;
-        switch (type)
+        try
         {
-            case device_type::SERIAL:
-                group = new serial_buzzergroup(device);
-                break;
-            case device_type::KEYBOARD:
-                group = new keyboard_buzzergroup(device);
-                break;
-        }
+            // If not connect it
+            unique_ptr<buzzergroup> group;
+            switch (type)
+            {
+                case device_type::SERIAL:
+                    group.reset(new serial_buzzergroup(device));
+                    break;
+                case device_type::KEYBOARD:
+                    group.reset(new keyboard_buzzergroup(device));
+                    break;
+            }
 
-        group->buzzer_connected.connect([this](buzzergroup &buzzergroup, unsigned char buzzer_id){this->buzzer_connected.raise(buzzergroup.get_id(), buzzer_id);});
-        group->buzzer_disconnected.connect([this](buzzergroup &buzzergroup, unsigned char buzzer_id){this->buzzer_disconnected.raise(buzzergroup.get_id(), buzzer_id);});
-        group->buzzer_hit.connect([this](buzzergroup &buzzergroup, unsigned char buzzer_id){this->buzzer_hit.raise(buzzergroup.get_id(), buzzer_id);});
-        group->buzzergroup_connected.connect([this](buzzergroup &buzzergroup, const std::set<unsigned char> &connected_buzzers){this->buzzergroup_connected.raise(buzzergroup.get_id(), connected_buzzers);});
-        group->buzzergroup_connect_failed.connect(bind(&buzzergroup_manager::on_buzzergroup_connect_failed, this, _1, _2));
-        group->buzzergroup_disconnected.connect(bind(&buzzergroup_manager::on_buzzergroup_disconnected, this, _1, _2));
-        group->start_threads();
-        buzzergroups.emplace(device, unique_ptr<buzzergroup>(group));
+            group->buzzer_connected.connect([this](buzzergroup &buzzergroup, unsigned char buzzer_id){this->buzzer_connected.raise(buzzergroup.get_id(), buzzer_id);});
+            group->buzzer_disconnected.connect([this](buzzergroup &buzzergroup, unsigned char buzzer_id){this->buzzer_disconnected.raise(buzzergroup.get_id(), buzzer_id);});
+            group->buzzer_hit.connect([this](buzzergroup &buzzergroup, unsigned char buzzer_id){this->buzzer_hit.raise(buzzergroup.get_id(), buzzer_id);});
+            group->buzzergroup_connected.connect([this](buzzergroup &buzzergroup, const std::set<unsigned char> &connected_buzzers){this->buzzergroup_connected.raise(buzzergroup.get_id(), connected_buzzers);});
+            group->buzzergroup_connect_failed.connect(bind(&buzzergroup_manager::on_buzzergroup_connect_failed, this, _1, _2));
+            group->buzzergroup_disconnected.connect(bind(&buzzergroup_manager::on_buzzergroup_disconnected, this, _1, _2));
+            group->start_threads();
+            buzzergroups.emplace(device, unique_ptr<buzzergroup>(group.release()));
+        }
+        catch (string &s)
+        {
+            buzzergroup_connect_failed.raise(device, s);
+        }
     }
 }
 
